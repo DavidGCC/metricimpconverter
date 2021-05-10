90c142b0934662dfec5d770b39e24f9a792f7ed9
DavidG <datog309@gmail.com>
2021-05-10 16:45:22 +0400

    number tests

--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -6,4 +6,38 @@ let convertHandler = new ConvertHandler();
 
 suite('Unit Tests', function(){
 
+    suite("convertHandler tests for numbers", () => {
+
+        test("convertHandler should correctly read a whole number", () => {
+            const input = "123kg";
+            assert.equal(convertHandler.getNum(input), 123);
+        });
+
+        test("convertHandler should correctly read a decimal number input", () => {
+            const input = "1.23L";
+            assert.equal(convertHandler.getNum(input), 1.23);
+        });
+
+        test("convertHandler should correctly read a fractional input", () => {
+            const input = "2/4lbs";
+            assert.equal(convertHandler.getNum(input), 0.5);
+        });
+
+        test("convertHandler should correctly read a fractional input with a decimal", () => {
+            const input = "6.5/2km";
+            assert.equal(convertHandler.getNum(input), 3.25);
+        });
+
+        test("convertHandler should correctly return an error on a double-fraction (i.e. 3/2/3)", () => {
+            const input = "3/2/3mi";
+            assert.equal(convertHandler.getNum(input), "invalid number");
+        });
+
+        test("convertHandler should correctly default to a numerical input of 1 when no numerical input is provided", () => {
+            const input = "gal";
+            assert.equal(convertHandler.getNum(input), 1);
+        });
+
+    });
+
 });
